b760c6c99d92af252f44c5660590aac69cf42a00
bykbyk0401 <bykbyk0401@gmail.com>
2024-10-16 23:11:20 +0900

    feat: localstorage 연습

--- week2/practice/todo.js
+++ week2/practice/todo.js
@@ -1,21 +1,61 @@
-const todoList = document.querySelector(".todo-list");
-
 const button = document.querySelector(".button");
 
-const buttonClick = () => {
-    const li = document.createElement("li");
-    const input = document.createElement("span");
-    input.textContent = document.querySelector("input").value;
-    const deleteBnt = document.createElement("button");
-    deleteBnt.textContent = "삭제";
-    li.appendChild(input);
-    li.appendChild(deleteBnt);
-    todoList.appendChild(li);
-    document.querySelector("input").value = "";
-    localStorage.setItem("input", JSON.stringify(input,value));
-    localStorage.setItem("button", JSON.stringify(deleteBnt));
+const input = document.querySelector("input")
 
-    deleteBnt.addEventListener("click", () => {todoList.removeChild(li)});
+const todoListData = JSON.parse(localStorage.getItem("todoList")) ?? [];
+
+// const buttonClick = () => {
+//     const li = document.createElement("li");
+//     const input = document.createElement("span");
+//     input.textContent = document.querySelector("input").value;
+//     const deleteBnt = document.createElement("button");
+//     deleteBnt.textContent = "삭제";
+//     li.appendChild(input);
+//     li.appendChild(deleteBnt);
+//     todoList.appendChild(li);
+//     document.querySelector("input").value = "";
+//     localStorage.setItem("input", JSON.stringify(input,value));
+//     localStorage.setItem("button", JSON.stringify(deleteBnt));
+
+//     deleteBnt.addEventListener("click", () => {todoList.removeChild(li)});
+// };
+
+const renderList = () => {
+    const todoList = document.querySelector(".todo-list");
+    todoList.innerHTML = "";
+    todoListData.forEach((todo, index) => {
+        const li = document.createElement("li");
+        const input = document.createElement("span");
+        const deleteBnt = document.createElement("button");
+        
+        input.textContent = todo;
+        deleteBnt.textContent = "삭제";
+        li.appendChild(input);
+        li.appendChild(deleteBnt);
+        todoList.appendChild(li);
+
+        deleteBnt.addEventListener("click", () => {
+            todoListData.splice(index, 1);
+            localStorage.setItem("todoList", JSON.stringify(todoListData));
+            renderList();
+        });
+    });
+}
+
+const buttonClick = () => {
+    const inputValue = input.value;
+    if(!inputValue) {
+        return;
+    }
+    todoListData.push(inputValue);
+    localStorage.setItem("todoList", JSON.stringify(todoListData));
+    renderList();
+    input.value = "";
 };
 
-button.addEventListener("click", buttonClick);
+const init = () => {
+    button.addEventListener("click", buttonClick);
+    renderList();
+};
+
+init();
