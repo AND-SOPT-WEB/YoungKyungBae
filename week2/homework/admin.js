a47fdfec626845f12ef5fa6caeca6aa97ebc6cef
bykbyk0401 <bykbyk0401@gmail.com>
2024-10-22 02:35:49 +0900

    feat: 체크박스 전체 및 부분 선택과 해제

--- week2/homework/admin.js
+++ week2/homework/admin.js
@@ -5,6 +5,8 @@ const membersData = JSON.parse(localStorage.getItem("membersData")) ?? [];
 const searchBtn = document.querySelector(".search");
 const resetBtn = document.querySelector(".reset");
 
+const deleteBnt = document.querySelector(".delete");
+
 const getInputValues = () => {
     return {
         name: document.querySelector("input.name").value.trim(),
@@ -17,11 +19,50 @@ const getInputValues = () => {
     };
 };
 
+const createTableHeader = () => {
+    const memberTable = document.querySelector(".table");
+
+    const memberTr = document.createElement("tr");
+    const checkBoxHead = document.createElement("th");
+    const checkBox = document.createElement("input");
+    const name = document.createElement("th");
+    const englishName = document.createElement("th");
+    const github = document.createElement("th");
+    const gender = document.createElement("th");
+    const role = document.createElement("th");
+    const firstWeekGroup = document.createElement("th");
+    const secondWeekGroup = document.createElement("th");
+
+    checkBoxHead.appendChild(checkBox);
+    checkBox.setAttribute("type", "checkbox");
+    checkBox.setAttribute("class", "checkAll");
+    name.textContent = "이름";
+    englishName.textContent = "영문이름";
+    github.textContent = "GitHub";
+    gender.textContent = "성별";
+    role.textContent = "역할";
+    firstWeekGroup.textContent = "1주차 금잔디";
+    secondWeekGroup.textContent = "2주차 금잔디";
+
+    memberTr.appendChild(checkBoxHead);
+    memberTr.appendChild(name);
+    memberTr.appendChild(englishName);
+    memberTr.appendChild(github);
+    memberTr.appendChild(gender);
+    memberTr.appendChild(role);
+    memberTr.appendChild(firstWeekGroup);
+    memberTr.appendChild(secondWeekGroup);
+    memberTable.appendChild(memberTr);
+};
+
 const renderList = (data) => {
     const memberTable = document.querySelector(".table");
     memberTable.innerHTML = "";
+    createTableHeader();
     data.forEach((member, index) => {
         const memberTr = document.createElement("tr");
+        const checkBoxHead = document.createElement("td");
+        const checkBox = document.createElement("input");
         const name = document.createElement("td");
         const englishName = document.createElement("td");
         const github = document.createElement("td");
@@ -30,6 +71,9 @@ const renderList = (data) => {
         const firstWeekGroup = document.createElement("td");
         const secondWeekGroup = document.createElement("td");
 
+        checkBoxHead.appendChild(checkBox);
+        checkBox.setAttribute("type", "checkbox");
+        checkBox.setAttribute("class", "checkBox");
         name.textContent = member.name;
         englishName.textContent = member.englishName;
         github.textContent = member.github;
@@ -38,6 +82,7 @@ const renderList = (data) => {
         firstWeekGroup.textContent = member.firstWeekGroup;
         secondWeekGroup.textContent = member.secondWeekGroup;
 
+        memberTr.appendChild(checkBoxHead);
         memberTr.appendChild(name);
         memberTr.appendChild(englishName);
         memberTr.appendChild(github);
@@ -46,7 +91,54 @@ const renderList = (data) => {
         memberTr.appendChild(firstWeekGroup);
         memberTr.appendChild(secondWeekGroup);
         memberTable.appendChild(memberTr);
+
+        deleteBnt.addEventListener("click", () => {
+            if(checkBox.checked) {
+                data.splice(index, 1);
+                localStorage.setItem("membersData", JSON.stringify(data));
+                renderList(data);
+            }
+        });
     });
+
+    const checkAll = document.querySelector(".checkAll");
+    checkAll.addEventListener("click", () => checkAllClick(checkAll));
+
+    const checkboxes = document.querySelectorAll('.checkBox');
+    for(const checkbox of checkboxes) {
+        checkbox.addEventListener("click", () => {
+            const totalCnt = checkboxes.length;
+            console.log(totalCnt);
+
+            const checkedCnt = document.querySelectorAll('.checkBox:checked').length;
+            
+            if(totalCnt === checkedCnt){
+                checkAll.checked = true;
+            }
+            else{
+            checkAll.checked = false;
+            }
+
+        });
+    }
+};
+
+const checkAllClick = (checkAll) => {
+    const isChecked = checkAll.checked;
+
+    if(isChecked){
+        const checkboxes = document.querySelectorAll('.checkBox');
+        for(const checkbox of checkboxes){
+            checkbox.checked = true;
+        }
+    }
+
+    else{
+        const checkboxes = document.querySelectorAll('.checkBox');
+        for(const checkbox of checkboxes){
+            checkbox.checked = false;
+        }
+    }
 };
 
 const searchClick = () => {
